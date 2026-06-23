dac3c7611fe3daac1e099bd6c38511066423dc33
Blessing Gumede <mfanafuthiphilasand@gmail.com>
2026-06-23 21:10:59 +0200

    Initial commit

---
+++ src/test/java/com/philasande/invoiceflow/InvoiceflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philasande.invoiceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
